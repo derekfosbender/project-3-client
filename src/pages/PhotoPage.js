4a74a424cf6b62d03f4b93b02a819747cc733c2d
John Doe <derekfoz19@gmail.com>
2023-08-12 11:06:34 -0400

    up to date

--- src/pages/PhotoPage.js
+++ src/pages/PhotoPage.js
@@ -1,37 +1,37 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
+
+
 
 const API_URL = "http://localhost:5005";
 
-function PhotoPage () {
-    const [photos, setPhotos] = useState([]);
+function PhotoPage() {
+  const [photos, setPhotos] = useState([]);
 
-    const getPhotos = () => {
-        axios
-        .get(`${API_URL}/photo/photos`)
-        .then((res) => {
-          const formattedPhotos = res.data.map((photos) => ({
-          }));
-            setPhotos(formattedPhotos);
-    })
-        .catch((err) => console.log(err));
-    }
+  const getPhotos = () => {
 
-    useEffect(() => {
-        getPhotos();
-    }, []);
+    axios
+      .get(`${API_URL}/photo`)
+      .then((res) => {
+        setPhotos(res.data.photos);
+        })
+      .catch((err) => console.log(err));
+  };
 
-    return (
-        <div className="PhotoPage">
-            {photos.map((photo) => (
-              <div key={photo._id} className="post">
-                <img src={photo.photo} className="ph" />
-                <h4>{photo.location}</h4>
-                </div>
-       ))}
-    
+  useEffect(() => {
+    getPhotos();
+  }, []);
+
+  return (
+    <div className="PhotoPage">
+      {photos.map((photo) => (
+        <div key={photo._id} className="post">
+          <img src={photo.photo} className="ph" alt={`Photo ${photo._id}`} />
+          <h4>{photo.location}</h4>
+        </div>
+      ))}
     </div>
-    )
+  );
 }
 
 export default PhotoPage;
